d72937f155ada6c88c60be30a0ecd8eda44a812b
Devan.M <devan19ct@users.noreply.github.com>
2025-06-24 14:45:40 +0530

    update slash tracker

--- slash_tracker.py
+++ slash_tracker.py
@@ -8,12 +8,17 @@ TRACKER_FILE = "slash_tracker.json"
 def update_slash_usage():
     ist = timezone("Asia/Kolkata")
     now_ist = datetime.now(ist)
+    print(f"[Slash used] {now_ist.isoformat()}")
+
     with open(TRACKER_FILE, "w") as f:
         json.dump({"last_used": now_ist.isoformat()}, f)
+        print(f"[File written] {TRACKER_FILE}")
 
 def get_last_slash_usage():
     if not os.path.exists(TRACKER_FILE):
         return None
     with open(TRACKER_FILE, "r") as f:
         data = json.load(f)
-        return datetime.fromisoformat(data["last_used"])
+        ist = timezone("Asia/Kolkata")
+        return datetime.fromisoformat(data["last_used"]).astimezone(ist)
+
